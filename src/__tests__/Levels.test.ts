fef5707af14a790a467718d766f8019a2c5d73b8
palehazy <austinrpg@live.com>
2022-11-12 03:27:33 -0600

    adding test

--- src/__tests__/Levels.test.ts
+++ src/__tests__/Levels.test.ts
@@ -1,5 +1,6 @@
 import json from "../../public/onelvl.json";
 import { DefaultLevel } from "../engine/Levels/Defaultlevel";
+import { RootLevel } from "../engine/Levels/RootLevel";
 import { touchAllLevels } from "../utils";
 
 jest.setTimeout(100000);
@@ -23,10 +24,45 @@ test("the level has a finished status when done", async () => {
   });
 });
 
-
 test("status is running right the start", async () => {
   const rootLevel = new DefaultLevel(json as any, {});
   const p = rootLevel.start();
   expect(rootLevel.status).toBe("running");
+  await p;
+});
+
+const level = {
+  levelsFlow: "async",
+  tasksFlow: "async",
+  name: "root",
+  delay: 0,
+  levels: [
+    {
+      levelsFlow: "async",
+      tasksFlow: "sync",
+      delay: 5000,
+      name: "root-level1",
+      levels: [],
+      tasks: [],
+    },
+  ],
+  tasks: [],
+};
+
+test("level delays appropriate time", async () => {
+  let time = performance.now();
+  const rootLevel = new RootLevel(level as any, {
+    events: {
+      onLevelStartDelay(level) {
+        time = performance.now();
+        expect(level.status).toBe("running");
+      },
+      onLevelFinishDelay(level) {
+        expect(performance.now() - time).toBeGreaterThan(5000);
+        expect(level.status).toBe("running");
+      }
+    },
+  });
+  const p = rootLevel.start();
   await p;
 });
